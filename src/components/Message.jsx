89a17a8e5d4b8f06bd3a625fc11ef04f224eac0a
Viva Dev <109100020+okpalavivian@users.noreply.github.com>
2023-03-18 23:03:17 +0000

    update

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 const Message =()=>{
     return(
-        <div>Message</div>
+    <div className="message">
+    <div className="messageInfo">
+            
+    <img src="c" alt="" />
+    <span>just now</span>
+
+    </div>
+    <div className="messageContent">
+  <p>helo</p>
+  <img src="" alt="" />
+            </div>
+          </div>
     )
 }
 export default Message
